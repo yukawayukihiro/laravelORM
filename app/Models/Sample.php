b9dac4f765ad40517cf30c75054b637e575130c6
yukawa <metalg9123@i.softbank.jp>
2023-04-25 20:35:04 +0900

    マイグレーションファイル作成

--- app/Models/Sample.php
+++ app/Models/Sample.php
@@ -9,12 +9,5 @@ class Sample extends Model
 {
     //モデルと関連しているテーブルの設定
     protected $table = 'sample';
-    //テーブルの主キーの設定
-    protected $primarykey = 'user_id';
-    //IDが自動増分されるかを設定
-    public $incrementing = false;
-    //モデルのタイムスタンプを更新するかの指示を設定
-    public $timestamps = false;
-    //代入しない属性を設定
-    protected $guarded = ['id'];   
+    
 }
